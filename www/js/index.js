ef1f209d7f4e06fb3949dece4b87d362f98925a7
simberaj <simbera.jan@gmail.com>
2015-12-16 13:56:03 +0100

    fixed map display, added sliding button interface

--- www/js/index.js
+++ www/js/index.js
@@ -5,15 +5,79 @@ var poiLayer = L.layerGroup();
 var remotePOIDB;
 var localPOIDB;
 
+function createButton(cls, container, options) {
+  but = L.DomUtil.create('a', cls, container);
+  but.href = '#';
+  for (opt in options) {
+    but[opt] = options[opt];
+  }
+  return but;
+}
+
+L.Control.CommandPanel = L.Control.extend({
+  options: {
+    // topright, topleft, bottomleft, bottomright
+    position: 'topright',
+    placeholder: 'Do'
+  },
+  initialize: function (options /*{ data: {...}  }*/) {
+    // constructor
+    L.Util.setOptions(this, options);
+  },
+  onAdd: function (map) {
+    // happens after added to map
+    console.log('<commandpanel-onadd>');
+    var container = L.DomUtil.create('div', 'command-container');
+    this.collapsed = L.DomUtil.create('div', 'leaflet-bar container-collapsed', container);
+    this.expandButton = createButton('command.button command-expand', this.collapsed, {title: 'More controls...'});
+    this.expandButton.onclick = this.expand;
+    // this.expandButton.innerHTML = 'v';
+    this.expanded = L.DomUtil.create('div', 'leaflet-bar container-expanded', container);
+    this.expanded.style.display = 'none';
+    // $('.container-expanded').hide();
+    this.collapseButton = createButton('command-button command-collapse', this.expanded, {title: 'Locate me'});
+    this.collapseButton.onclick = this.collapse;
+    this.locateButton = createButton('command-button command-locate', this.expanded, {title: 'Locate me'});
+    this.layerButton = createButton('command-button command-layer', this.expanded, {title: 'Choose what to display'});
+    this.searchButton = createButton('command-button command-search', this.expanded, {title: 'Search'});
+    this.addButton = createButton('command-button command-add', this.expanded, {title: 'Add place'});
+    this.optionsButton = createButton('command-button command-options', this.expanded, {title: 'Options'});
+    // L.DomEvent.addListener(this.input, 'keyup', _.debounce(this.keyup, 300), this);
+    // L.DomEvent.addListener(this.form, 'submit', this.submit, this);
+    L.DomEvent.disableClickPropagation(container);
+    console.log('</commandpanel-onadd>');
+    return container;
+  },
+  onRemove: function (map) {
+    // when removed
+    // L.DomEvent.removeListener(this._input, 'keyup', this.keyup, this);
+    // L.DomEvent.removeListener(form, 'submit', this.submit, this);
+  },
+  expand: function (evt) {
+    $('.container-collapsed').hide();
+    $('.container-expanded').show();
+  },
+  collapse: function (evt) {
+    $('.container-collapsed').show();
+    $('.container-expanded').hide();
+  },
+});
+
+L.control.commandpanel = function(id, options) {
+  return new L.Control.CommandPanel(id, options);
+};
+
+
 function deviceIsReady() {
   console.log('<deviceIsReady>');
-  $('#page-map').one('pageshow', initMap);
+  // $('#page-map').one('pageshow', initMap);
   $('#add-mode-off').hide();
   // $.one('pageshow', initMap);
   // db setup
   remotePOIDB = new PouchDB('http://gi88.geoinfo.tuwien.ac.at:5984/refu');
   localPOIDB = new PouchDB('mappoint');
   initSync();
+  initMap();
   console.log('</deviceIsReady>');
 }
 
@@ -22,9 +86,12 @@ function initMap() {
   map = L.map('map');
   // tiles
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    maxZoom: 16,
+    maxZoom: 17,
     attribution: '&copy; <a href="http://osm.org">OSM contributors</a>'
   }).addTo(map);
+  L.control.scale({imperial : false}).addTo(map);
+  L.control.commandpanel().addTo(map);
+  // map.addControl(new controlPanel());
   // locate the device
   map.locate({setView: true, maxZoom: 16});
   // add the POIs
@@ -134,37 +201,3 @@ function drawPOIs() {
         console.log(error);
     });
 }
-
-// var app = {
-    // // Application Constructor
-    // initialize: function() {
-        // this.bindEvents();
-    // },
-    // // Bind Event Listeners
-    // //
-    // // Bind any events that are required on startup. Common events are:
-    // // 'load', 'deviceready', 'offline', and 'online'.
-    // bindEvents: function() {
-        
-    // },
-    // // deviceready Event Handler
-    // //
-    // // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // // function, we must explicitly call 'app.receivedEvent(...);'
-    // onDeviceReady: function() {
-        // app.receivedEvent('deviceready');
-    // },
-    // // Update DOM on a Received Event
-    // receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        // console.log('Received Event: ' + id);
-    // }
-// };
-
-// app.initialize();
