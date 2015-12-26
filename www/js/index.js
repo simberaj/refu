77b42357dc3f3e1b31c34333b3df82b1473babfa
simberaj <simbera.jan@gmail.com>
2015-12-26 14:31:15 +0100

    basic page structure, button navigation, locate me function

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,8 @@ var poiLayer = L.layerGroup();
 var remotePOIDB;
 var localPOIDB;
 
+addMode = false;
+
 function createButton(cls, container, options) {
   but = L.DomUtil.create('a', cls, container);
   but.href = '#';
@@ -29,19 +31,21 @@ L.Control.CommandPanel = L.Control.extend({
     console.log('<commandpanel-onadd>');
     var container = L.DomUtil.create('div', 'command-container');
     this.collapsed = L.DomUtil.create('div', 'leaflet-bar container-collapsed', container);
-    this.expandButton = createButton('command.button command-expand', this.collapsed, {title: 'More controls...'});
+    this.expandButton = createButton('command-button command-expand', this.collapsed, {title: 'More controls...'});
     this.expandButton.onclick = this.expand;
     // this.expandButton.innerHTML = 'v';
     this.expanded = L.DomUtil.create('div', 'leaflet-bar container-expanded', container);
     this.expanded.style.display = 'none';
     // $('.container-expanded').hide();
-    this.collapseButton = createButton('command-button command-collapse', this.expanded, {title: 'Locate me'});
+    this.collapseButton = createButton('command-button command-collapse', this.expanded, {title: 'Hide controls'});
     this.collapseButton.onclick = this.collapse;
     this.locateButton = createButton('command-button command-locate', this.expanded, {title: 'Locate me'});
+    this.locateButton.onclick = gotoLocation;
     this.layerButton = createButton('command-button command-layer', this.expanded, {title: 'Choose what to display'});
-    this.searchButton = createButton('command-button command-search', this.expanded, {title: 'Search'});
+    this.searchButton = createButton('command-button command-search', this.expanded, {title: 'Search', href: '#page-search'});
     this.addButton = createButton('command-button command-add', this.expanded, {title: 'Add place'});
-    this.optionsButton = createButton('command-button command-options', this.expanded, {title: 'Options'});
+    this.addButton.onclick = switchAddMode;
+    this.optionsButton = createButton('command-button command-options', this.expanded, {title: 'Options', href: '#page-options'});
     // L.DomEvent.addListener(this.input, 'keyup', _.debounce(this.keyup, 300), this);
     // L.DomEvent.addListener(this.form, 'submit', this.submit, this);
     L.DomEvent.disableClickPropagation(container);
@@ -87,17 +91,21 @@ function initMap() {
   // tiles
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
-    attribution: '&copy; <a href="http://osm.org">OSM contributors</a>'
+    attribution: '&copy; <a href="http://osm.org">OSM contributors</a> | <a href="#page-about">Project Refu</a>'
   }).addTo(map);
   L.control.scale({imperial : false}).addTo(map);
   L.control.commandpanel().addTo(map);
   // map.addControl(new controlPanel());
   // locate the device
-  map.locate({setView: true, maxZoom: 16});
+  gotoLocation();
   // add the POIs
   poiLayer.addTo(map);
   drawPOIs();
   console.log('</initMap>');
+}
+
+function gotoLocation() {
+  map.locate({setView: true, maxZoom: 16});
 }
 
 function initSync() {
@@ -145,28 +153,48 @@ function setStatusIcon(state) {
   }
 }
 
+function switchAddMode() {
+  if (addMode) {
+    addModeOff();
+  } else {
+    addModeOn();
+  }
+}
+
 function addModeOn() {
+  console.log('<addmode=true>');
   map.on('click', addPlace);
   $('#add-mode-off').show();
-  $('#add-mode-on').hide();  
+  $('#add-mode-on').hide();
+  $('.command-add').toggleClass('command-button', false);
+  $('.command-add').toggleClass('command-button-down', true);
+  $('#map').toggleClass('locselecting', true);
+  addMode = true;
+  console.log('</addmode=true>');
 }
 
 function addModeOff() {
+  console.log('<addmode=false>');
   map.removeEventListener('click');
   $('#add-mode-off').hide();
-  $('#add-mode-on').show();  
+  $('#add-mode-on').show();
+  $('.command-add').toggleClass('command-button', true);
+  $('.command-add').toggleClass('command-button-down', false);
+  $('#map').toggleClass('locselecting', false);
+  addMode = false;
+  console.log('</addmode=false>');
 }
 
 function addPlace(evt) {
-  console.log(evt);
+  console.log('<addplace>');
   var name = prompt("Name of the Place:");
-  if (name != "") {
-    var creator = device.uuid;
-    if (creator == undefined) {
-      creator = 'browser';
-    }
-    newDBEntry([evt.latlng.lat, evt.latlng.lng], {"name" : name, "creator" : creator});
+  if (name == undefined || name == "" || name == null)
+    return;
+  var creator = device.uuid;
+  if (creator == undefined) {
+    creator = 'browser';
   }
+  newDBEntry([evt.latlng.lat, evt.latlng.lng], {"name" : name, "creator" : creator});
   addModeOff();
 }
 
@@ -176,6 +204,7 @@ function newDBEntry(coor, props) {
     "geometry" : {"type" : "Point", "coordinates" : coor},
     "properties" : props
   };
+  console.log(doc);
   localPOIDB.put(doc).then(refresh).catch(errorStoring);
 }
 
@@ -195,9 +224,19 @@ function drawPOIs() {
       for (var entrynumber in results.rows) {
         var entry = results.rows[entrynumber];
         var marker = L.marker(entry.value.geometry.coordinates).addTo(poiLayer);
-        marker.bindPopup(entry.value.properties.name);
+        // var content = '<a class="popupname">' + entry.value.properties.name + '</a><div href="#" class="inpopup popup-details" onclick="showDetails(\'' + entry.value.properties.name + '\');"></div><div class="inpopup popup-close"><a href="#close"></a></div>';
+        var content = '<p class="popupname">' + entry.value.properties.name + '</p><a class="morelink" href="#" onclick="showDetails(\'' + entry.value.properties.name + '\');">...</a>';
+        // marker.bindPopup(L.popup({closeButton: false}).setContent(content));
+        // marker.bindPopup(entry.value.properties.name);
+        marker.bindPopup(content);
       };
     }).catch(function (error) {
         console.log(error);
     });
+}
+
+function showDetails(name) {
+  document.getElementById('details-name').innerHTML = name;
+  // map.closePopup();
+  window.location = '#page-details';
 }
